c6c84107bc18c8c4c651f0a6956be2c8a09552bd
Sivamurugan <94527948+Siva-SMR@users.noreply.github.com>
2022-04-04 15:35:58 +0530

    Update movie.sql

--- movie.sql
+++ movie.sql
@@ -1,20 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Mar 21, 2022 at 09:33 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `movie`
@@ -27,42 +10,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `actors` (
-  `actor` varchar(20) NOT NULL,
-  `ayear` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `actor` varchar(30) NOT NULL,
+  `ayear` year(4) NOT NULL
+)
 
 --
 -- Dumping data for table `actors`
 --
 
 INSERT INTO `actors` (`actor`, `ayear`) VALUES
-('cage', 1964),
-('Hanks', 1956),
-('Maguire', 1975),
+('Cage', 1964),
+('HANKS', 1956),
+('Magiure', 1975),
 ('McDormand', 1957);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `acts`
---
-
-CREATE TABLE `acts` (
-  `actors` varchar(20) NOT NULL,
-  `title` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `acts`
---
-
-INSERT INTO `acts` (`actors`, `title`) VALUES
-('Cage', 'Raising Arizona'),
-('Maguire', 'Spiderman'),
-('Maguire', 'Wonder Boys'),
-('McDormand', 'Fargo'),
-('McDormand', 'Raising Arizona'),
-('McDormand', 'Wonder Boys');
 
 -- --------------------------------------------------------
 
@@ -71,9 +31,9 @@ INSERT INTO `acts` (`actors`, `title`) VALUES
 --
 
 CREATE TABLE `directors` (
-  `director` varchar(20) NOT NULL,
-  `dyear` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `director` varchar(30) NOT NULL,
+  `dyear` year(4) NOT NULL
+)
 
 --
 -- Dumping data for table `directors`
@@ -91,11 +51,11 @@ INSERT INTO `directors` (`director`, `dyear`) VALUES
 --
 
 CREATE TABLE `movies` (
-  `title` varchar(20) NOT NULL,
-  `director` varchar(20) NOT NULL,
-  `myear` int(11) NOT NULL,
-  `rating` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `title` varchar(50) NOT NULL,
+  `director` varchar(30) DEFAULT NULL,
+  `myear` year(4) DEFAULT NULL,
+  `rating` float DEFAULT NULL
+)
 
 --
 -- Dumping data for table `movies`
@@ -103,48 +63,6 @@ CREATE TABLE `movies` (
 
 INSERT INTO `movies` (`title`, `director`, `myear`, `rating`) VALUES
 ('Fargo', 'Coen', 1996, 8.2),
-('Raising Arizona', 'Coen', 1987, 7.6),
+('Raising', 'Coen', 1987, 7.6),
 ('Spiderman', 'Raimi', 2002, 7.4),
 ('Wonder Boys', 'Hanson', 2000, 7.6);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `actors`
---
-ALTER TABLE `actors`
-  ADD PRIMARY KEY (`actor`);
-
---
--- Indexes for table `acts`
---
-ALTER TABLE `acts`
-  ADD KEY `actors` (`actors`);
-
---
--- Indexes for table `directors`
---
-ALTER TABLE `directors`
-  ADD PRIMARY KEY (`director`);
-
---
--- Indexes for table `movies`
---
-ALTER TABLE `movies`
-  ADD PRIMARY KEY (`title`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `acts`
---
-ALTER TABLE `acts`
-  ADD CONSTRAINT `acts_ibfk_1` FOREIGN KEY (`actors`) REFERENCES `actors` (`actor`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
